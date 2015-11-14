56b64caad769aca23a970ba1ec4bf12c06bfe24b
Roman Malakhov <romaroma@zoom-group.ru>
2015-11-15 03:22:13 +0600

    Simple REST API logger added

--- main.js
+++ main.js
@@ -1,1 +1,18 @@
-console.log("Hello, world!");
+// Init stuff
+var http = require('http');
+
+// Log the plate's launch
+notifyAPI("launch", 1);
+
+
+function notifyAPI(msgType, value) {
+    // Yes, it is hardcoded, baby!
+    var deviceId = "123";
+    var url = "http://example.com/api/" + deviceId + "/" + msgType + "/" + value;
+
+    http.get(url, function(res) {
+      console.log("Response from API: " + res.statusCode);
+    }).on('error', function(e) {
+      console.log("Error from API: " + e.message);
+    });
+}
