f3ae8d6d076171c9c0b6064947e81e6f9c9a2545
Sam Sheffres <ssheffres@gmail.com>
2015-11-18 18:27:24 +0600

    Use Parse

--- main.js
+++ main.js
@@ -9,6 +9,9 @@ var temp = new groveSensor.GroveTemp(2); // temperature
 var upmMicrophone = require("jsupm_mic"); // gimme the mic calls
 var myMic = new upmMicrophone.Microphone(0);
 var threshContext = new upmMicrophone.thresholdContext();
+var Parse = require('parse/node').Parse;
+
+Parse.initialize("Few1XXtjnoqhyNLOqgEz63H4GOg2IonFIlLEXVKy", "K0JVKCTYrWMuG3Qszg2jsV4TjlXwxDvLi7gksyJt");
 
 // store last values that we sent to API to reduce
 // amount of duplicate date to be sent again
@@ -65,8 +68,9 @@ function getNoise(threshold) {
     threshContext.averagedOver = 2;
     
     // listen to the music (and noise) for 2*100 ms
-    var buffer = new upmMicrophone.uint16Array(100);
-    var len = myMic.getSampledWindow(2, 100, buffer);
+    var buffer = new upmMicrophone.uint16Array(5000);
+    
+    var len = myMic.getSampledWindow(2, 5000, buffer);
     
     if (len) {
         return myMic.findThreshold(threshContext, threshold, buffer, len);
@@ -89,21 +93,24 @@ function notifyAPI(msgType, value) {
         apiCache[msgType] = value;
     }
     
-    // Yes, it is hardcoded, baby!
-    var deviceId = "1";
-    var url = "http://146.148.120.81/api/" + deviceId + "/" + msgType + "/" + value;
+    var Alert = Parse.Object.extend("Alert");
+    var alert = new Alert();
 
-    console.log(url);
-    try {
-        http.get(url, function(res) {
-          //console.log("Response from API: " + res.statusCode);
-        }).on('error', function(e) {
-          console.log("Error from API: " + e.message);
-        });
-    }
-    catch (e) {
-        console.log("HTTP error");
-    }
+    alert.set("device_id", 1);
+    alert.set("event_type", msgType);
+    alert.set("amount", value);
+
+    alert.save(null, {
+      success: function(alert) {
+        // Execute any logic that should take place after the object is saved.
+        alert('New object created with objectId: ' + alert.id);
+      },
+      error: function(alert, error) {
+        // Execute any logic that should take place if the save fails.
+        // error is a Parse.Error with an error code and message.
+        alert('Failed to create new object, with error code: ' + error.message);
+      }
+    });
 }
 
 // Handling async HTTP errors
